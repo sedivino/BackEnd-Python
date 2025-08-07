cd5fd050cb62b993f2d69c4916fdc9dd0e40da73
Eduardo Divino <eduardo.divino@discente.ufg.br>
2025-08-07 14:40:33 -0300

    Boas vindas

--- aula20250807.py
+++ aula20250807.py
@@ -1,2 +1,15 @@
 # This is a simple Python script to print "Hello world!"
 print("Hello world!")
+print("--")
+print()
+
+# Additional functionality can be added here
+# For example, we can define a function to greet a user
+def greet_user(name):
+    """Function to greet a user by name."""
+    print(f"Hello, {name}!")
+
+# Call the function with a sample name
+name = input("Enter your name: ")
+greet_user(name)
+print("--")
