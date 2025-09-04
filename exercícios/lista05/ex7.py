43cdb0766aa262b444c4a800a0411dd44f181a1f
Eduardo Divino <eduardo.divino@discente.ufg.br>
2025-09-04 16:28:18 -0300

    Lista 05 Ex 7 Atualização

--- exercícios/lista05/ex7.py
+++ exercícios/lista05/ex7.py
@@ -1,6 +1,7 @@
 # Faça um programa que leia dois vetores de dez elementos inteiros cada um e mostre um vetor
 # resultante da intercalação desses dois vetores.
 
+'''
 # Criando os vetores e o vetor resultante:
 vetor1 = []
 vetor2 = []
@@ -17,10 +18,16 @@ for i in range(10):
     vetor2.append(num2)
 
     vetor_resultante.append(num2)
+'''
+# Criando os vetores e o vetor resultante:
+vetor1 = [3, 5, 4, 2, 2, 5, 3, 2, 5, 9]
+vetor2 = [3, 5, 4, 2, 2, 5, 3, 2, 5, 9]
+vetor_resultante = []
 
 # Intercalando os vetores:
 for i in range(10):
-    vetor_resultante.insert(2*i, vetor1[i])
+    vetor_resultante.append(vetor1[i])
+    vetor_resultante.append(vetor2[i])
 
 # Mostrando o vetor resultante:
 print("Vetor resultante da intercalação:")
